731780f88016cbbc8e7b72efd141e2b5ca064f63
Andrew <24258342+acodeguy@users.noreply.github.com>
2019-03-26 13:57:22 +0000

    pass feature test for enforcing password being 6 chars or longer

--- spec/features/registration_spec.rb
+++ spec/features/registration_spec.rb
@@ -12,7 +12,7 @@ RSpec.feature "Registration", type: :feature do
     expect(page).to have_content("Welcome! You have signed up successfully")
   end
 
-  scenario 'Password must be between 6 and 10 characters in length' do
+  scenario 'Password must be fewer than 10 characters in length' do
 
     visit "/users/sign_up"
     
@@ -22,6 +22,19 @@ RSpec.feature "Registration", type: :feature do
 
     click_button "Sign up"
     expect(page).to have_content('Password is too long (maximum is 10 characters)')
+
+  end
+
+  scenario 'Password must be more than 6 characters in length' do
+
+    visit "/users/sign_up"
+    
+    fill_in "user_email", with: "simon@simon.com"
+    fill_in "user_password", with: "sec"
+    fill_in "user_password_confirmation", with: "sec"
+
+    click_button "Sign up"
+    expect(page).to have_content('Password is too short (minimum is 6 characters)')
 
   end
 
