56203decb2ea58f6b325be89eca328dfddccb63a
James Ault <james.ault@johnlewis.co.uk>
2019-03-27 15:18:47 +0000

    So that a user can more easily read their post, allow multiline post creation

--- spec/features/user_can_submit_posts_spec.rb
+++ spec/features/user_can_submit_posts_spec.rb
@@ -10,11 +10,20 @@ RSpec.feature "Timeline", type: :feature do
     expect(page).to have_content("Hello, world!")
   end
 
-  scenario "Can submit posts and view them" do
+  scenario "Can submit multiple posts and view them in descending date" do
     sign_up_and_sign_in
     create_post(message: 'This is the first post')
     create_post(message: 'This is the second post')
     expect(page.first('.post')).to have_content('This is the second post')
+  end
+
+  scenario "Can submit a submit and view a post with multiple lines" do
+    sign_up_and_sign_in
+    visit "/posts"
+    click_link "New post"
+    fill_in "Message", with: "Hello, world!\nI am on a new line"
+    click_button "Submit" 
+    expect(page).to have_text("Hello, world!\nI am on a new line", :exact)
   end
 
 end
