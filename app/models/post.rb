451f025738104f2da2ad00ccf65d0ab32e484818
Sarah Byrne <sarah.byrne@johnlewis.co.uk>
2019-03-27 15:29:49 +0000

    add belongs_to? method to Post model to check whether a post belongs to a given user.  This is needed to ensure that users can only update their own posts

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,7 @@
 class Post < ApplicationRecord
   belongs_to :user
+
+  def belongs_to?(person) 
+    return (user == person)
+  end
 end
--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -3,4 +3,11 @@ require 'rails_helper'
 RSpec.describe Post, type: :model do
   it { is_expected.to be }
   it { should belong_to(:user) }
+
+  it('knows whether it belongs to a given user') do
+    user = User.create(email: "s@s.com", password: "111111", password_confirmation: "111111")
+    post = Post.new(:message => 'a dull post', :user => user)
+    expect(post.belongs_to?(99)).to be(false)
+    expect(post.belongs_to?(user)).to be(true)
+  end
 end
