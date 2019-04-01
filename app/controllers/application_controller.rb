863a0b0ab139291996c0acbcb130a9b3b84295ca
Simon <01secn@gmail.com>
2019-04-01 14:37:23 +0100

    Registration and login changed to work with SPA by using JSON tokens using device token auth, and all routes now validated this way (IE calls to api must pass token etc. in the header

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-        include DeviseTokenAuth::Concerns::SetUserByToken
+  include DeviseTokenAuth::Concerns::SetUserByToken
   protect_from_forgery with: :null_session, only: Proc.new { |c| c.request.format.json? }
-  before_action :authenticate_user!
+  before_action :authenticate_user!, except: [:new, :create]
 end
