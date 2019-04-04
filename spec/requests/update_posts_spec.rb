087fe239616a9f79eee2f7b06445d59033288b95
Andrew <24258342+acodeguy@users.noreply.github.com>
2019-04-04 15:23:50 +0100

    update posts api accepts requests to update posts

--- spec/requests/update_posts_spec.rb
+++ spec/requests/update_posts_spec.rb
@@ -1,29 +1,26 @@
 require 'rails_helper'
 
-RSpec.feature "Update Posts", type: :feature do
-  scenario "Can update a post from the timeline" do
-    user = create_user_and_sign_in
-    create_then_view_posts(message: 'This is a post', user_id: user.id)
+RSpec.feature "Update Posts", type: :request do
 
-    click_button "Update"
-    fill_in "Message", with: 'Post updated'
-    click_button "Submit" 
-    
-    expect(page).to have_content('Post updated')
+  before do
+    user_auth = register
+
+    Post.create(message: "A post, yo!", user_id: user_auth[:user_id])
+
+    @posts = Post.all
+    @id = @posts.first.id
+
+    patch "/posts/#{@id}", params: { 
+      post: { message: 'Post updated', id: @id }, 
+      'access-token': user_auth[:token], 
+      client: user_auth[:client], 
+      uid: user_auth[:email] 
+    }
+
+    @post = Post.find(@id)
   end
 
-  scenario 'Cannot update the posts of other users' do
-    user1 = create_user(email: 'user1@example.com', password: 'password')
-    create_then_view_posts(message: 'User One has written this.', user_id: user1.id)
-    create_user_and_sign_in
-    expect(page).not_to have_selector('.update')
-  end
-
-  scenario 'Cannot update a post after 10 minutes' do
-    user = create_user_and_sign_in
-    ten_mins_ago = DateTime.now - (1 / 24.0 / 6)
-    Post.create(message: 'Message with a given timestamp', user_id: user.id, created_at: ten_mins_ago)
-    visit '/posts'
-    expect(page).not_to have_selector('.update')
+  scenario "Can update a post from the timeline" do 
+    expect(@post.message).to eq 'Post updated'
   end
 end
